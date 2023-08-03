fd74e0fc15b6c637a6ceebd87904e1dfb169373a
Md Arif <aatif149209@gmail.com>
2023-08-03 16:26:41 +0530

    commit it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Signup from "./Signup";
 import Login from "./Login";
 import Home from "./Home";
 import Users from "./Users";
+import Error from "./Error";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/register" element={<Signup />}></Route>
           <Route path="/home" element={<Home />}></Route>
           <Route path="/users" element={<Users />}></Route>
+          <Route path="*" element={<Error />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
