c370e9e87580cb767095c201fb59287882b5cec8
Md Arif <aatif149209@gmail.com>
2023-08-03 17:22:23 +0530

    commit it

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Login from "./Login";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
@@ -9,10 +11,20 @@ const Users = () => {
       .then((users) => setUsers(users.data))
       .catch((err) => console.log(err));
   }, []);
+
+  const handleLogout = () => {
+    // Clear the user authentication data from local storage
+    localStorage.removeItem("userName");
+    useNavigate("/login");
+    // Assuming you store the authentication token as 'token'
+    // Perform any additional cleanup on the frontend (e.g., clear user data, etc.)
+    // Redirect the user to the login page or update the state to handle logged-out state
+  };
   return (
     <div className="container">
       <h1>
         <b>Registered User</b>
+        <button onClick={handleLogout}>Logout</button>
       </h1>
       <br />
       <br />
