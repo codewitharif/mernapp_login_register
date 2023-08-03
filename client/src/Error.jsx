ef484ae2b47608ab5d5b258affa8d25b3a4c6f55
Md Arif <aatif149209@gmail.com>
2023-08-03 16:55:21 +0530

    commit it

--- client/src/Error.jsx
+++ client/src/Error.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "./App.css";
 
 const Error = () => {
   return (
     <div className="container">
-      <h1>Page Not Found ...</h1>
+      <h1>
+        <span>Page Not Found - 404</span>
+      </h1>
     </div>
   );
 };
